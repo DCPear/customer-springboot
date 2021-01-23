5d07d2e2bcb5601596fd76bc00a0a922660e767d
Deepa Perera (DCPear) <dcpeartree@gmail.com>
2021-01-23 13:09:24 +1300

    add unit test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -22,12 +22,13 @@ insert into customer(first_Name, last_Name,  dob,  email, phone, address, level)
 
 -- Security data
 -- USER
--- hashed password: mypassword
+-- hashed password: MyPassword
 INSERT INTO security_user (id, username, password, first_name, last_name) VALUES
-(1,  'admin', '$2a$12$tkAwgt7IPcRHrhXGeOQIwehIe0t4m8yPxAe56.kqrCUSk6Xhpxada', 'Administrator', 'Adminstrator'),
-(2,  'Hazel', '$2a$12$tkAwgt7IPcRHrhXGeOQIwehIe0t4m8yPxAe56.kqrCUSk6Xhpxada', 'Hazel', 'Nutt'),
-(3,  'Clark', '$2a$12$tkAwgt7IPcRHrhXGeOQIwehIe0t4m8yPxAe56.kqrCUSk6Xhpxada', 'Clark', 'Kent'),
-(4,  'Frank', '$2a$12$tkAwgt7IPcRHrhXGeOQIwehIe0t4m8yPxAe56.kqrCUSk6Xhpxada', 'Frank', 'Furter');
+(1,  'admin', '$2y$12$LtJY9Q5.U3IK0jBeZokkueMzbbWjMVSE2uN49GkeX.fGGIA5QBRUe', 'Administrator', 'Adminstrator'),
+(2,  'Hazel', '$2y$12$LtJY9Q5.U3IK0jBeZokkueMzbbWjMVSE2uN49GkeX.fGGIA5QBRUe', 'Hazel', 'Nutt'),
+(3,  'Clark', '$2y$12$LtJY9Q5.U3IK0jBeZokkueMzbbWjMVSE2uN49GkeX.fGGIA5QBRUe', 'Clark', 'Kent'),
+(4,  'Frank', '$2y$12$LtJY9Q5.U3IK0jBeZokkueMzbbWjMVSE2uN49GkeX.fGGIA5QBRUe', 'Frank', 'Furter');
+
 -- ROLES
 
 INSERT INTO security_role (id, role_name, description) VALUES (1, 'ROLE_ADMIN', 'Administrator');
